b3e475b8a2b56fbf41c3b057d32fedd90f629f45
Shiv kumar <shivswarnkar114@gmail.com>
2025-08-14 13:26:15 +0530

    Add files via upload
    Learning Python

---
+++ problem1.py
@@ -0,0 +1,7 @@
+print('''twinkle twinkle little star how are you wonder what you are up aboove the world so high like a diamond in the sky
+      ''')
+
+print("''twinkle twinkle little star how are you wonder what you are up above ythe world so hiiggh like a diamonnd in the sky""")
+
+
+ 
---
+++ problem2.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("""  i want a 90lpa job in 1st semester""")
+engine.runAndWait()
