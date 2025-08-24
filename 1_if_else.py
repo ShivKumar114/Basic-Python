efb518ef1a24ccb671d51b9305f93a1599560f40
Shiv kumar <shivswarnkar114@gmail.com>
2025-08-25 00:47:26 +0530

    Add files via upload

---
+++ 1_if_else.py
@@ -0,0 +1,21 @@
+#  IF ELSE STATEMENT ARE A MULTIWAY DECISION TAKEN BY OUR PROGRAM DUE TO CERTAIN CONDITIONS IN OUR CODE.
+# AGAR HAM KOI V COMMAND DMEIN if ,else LAGAKAR JAISE ENTER KRENGE WO HAME NEEXT LINE MEIN THORA SA GAP DE DEGA.
+# if STATEMMENT AKELA AA SKTA HAI ELSE STATEMENT AKELA NHI AA SKTA.
+# THERE CAN BE ANY NUMBER OF ELIF STATEMENTS.
+# ELSE IS EXECUTED ONLY IF ALL THE CONDITIONS INSIDE ELIFS FAIL.
+
+#1)- IF ELSE STATEMENT USE.
+a = int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+else:
+    print("Restricted , Leave this site now. \nYou are below the age of consent.")
+    
+
+
+
+
+
