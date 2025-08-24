51c9458b2df9c3552fb155300464f8e568b4d183
Shiv kumar <shivswarnkar114@gmail.com>
2025-08-25 00:48:35 +0530

    Add files via upload

---
+++ 4-logical_operators.py
@@ -0,0 +1,4 @@
+# LOGICAL OPERATORS OPERATE ON CONDITIONAL STATEMENTS.
+# (1) and - true if both operands are true and false
+# (2) or - true if at least one operand is true or else false
+# (3) not - inverts true to false and false to true 
