5d7e8b3cb0582853151d3ab3b557eae848f460a7
Shiv kumar <shivswarnkar114@gmail.com>
2025-08-27 02:27:45 +0530

    Add files via upload

---
+++ 1_loops.py
@@ -0,0 +1,23 @@
+# iteration = i ka value.
+# 
+# Loops ka kamm hota hai ek kamm ko barr barr repeat krna bina code ko dobara likhe 
+# Loops type = for loop , while loop
+# for loop - range/sequence ke liye
+# while loop - condition ke liye
+
+#Q1)-  1 SE 5 TK PRINT KRNA HAI .
+#  METHOD 1} BY USING PRINT FUNCTION.
+print(1)
+print(2)    
+print(3)
+print(4)
+print(5)
+
+# METHOD 2} BY USING LOOPS.
+for i in range(1,6):        # (1,6) likhe hai output mein  1 se 5 tk ayyega 6 include nhi hoga.
+    print(i)
+
+    
+
+
+    
